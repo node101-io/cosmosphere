db8941ae227272d883bde42439e363a511adba5d
Necip <sagiroglun22@itu.edu.tr>
2023-09-29 18:25:46 +0300

    feat: sponsors draft

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -130,6 +130,149 @@ const schedule = [
   },
 ];
 
+const sponsors = {
+  developer_community_partners: {
+    name: 'Developer Community Partners',
+    logos: [
+      {
+        name: 'Rise in',
+        image: '/img/partners/rise-in.png',
+        linkedin: 'https://www.linkedin.com/company/risein/',
+        x: 'https://x.com/risein',
+      },
+    ]
+  },
+  community_partners: {
+    name: 'Community Partners',
+    logos: [
+      {
+        name: 'Testnetrun',
+        image: '/img/partners/testnetrun.png',
+        linkedin: 'https://www.linkedin.com/company/testnetrun/',
+        x: 'https://x.com/testnetrun',
+      },
+      {
+        name: 'Hoodies',
+        image: '/img/partners/hoodies.png',
+        linkedin: 'https://www.linkedin.com/company/hoodies/',
+        x: 'https://x.com/hoodies',
+      },
+      {
+        name: 'Altcointurk',
+        image: '/img/partners/altcointurk.png',
+        linkedin: 'https://www.linkedin.com/company/altcointurk/',
+        x: 'https://x.com/altcointurk',
+      },
+      {
+        name: 'Wilds',
+        image: '/img/partners/wilds.png',
+        linkedin: 'https://www.linkedin.com/company/wilds/',
+        x: 'https://x.com/wilds',
+      },
+      {
+        name: 'Huginn',
+        image: '/img/partners/huginn.png',
+        linkedin: 'https://www.linkedin.com/company/huginn/',
+        x: 'https://x.com/huginn',
+      },
+      {
+        name: 'Unive Chain',
+        image: '/img/partners/univechain.png',
+        linkedin: 'https://www.linkedin.com/company/univechain/',
+        x: 'https://x.com/univechain',
+      },
+      {
+        name: 'Imperator',
+        image: '/img/partners/imperator.png',
+        linkedin: 'https://www.linkedin.com/company/imperator/',
+        x: 'https://x.com/imperator',
+      },
+      {
+        name: 'Cosmic Turks',
+        image: '/img/partners/cosmic-turks.png',
+        linkedin: 'https://www.linkedin.com/company/cosmic-turks/',
+        x: 'https://x.com/cosmic-turks',
+      },
+    ]
+  },
+  talent_partners: {
+    name: 'Talent Partners',
+    logos: [
+      {
+        name: 'ITU Blockchain',
+        image: '/img/partners/itu-blockchain.png',
+        linkedin: 'https://www.linkedin.com/company/itu-blockchain/',
+        x: 'https://x.com/itu-blockchain',
+      },
+      {
+        name: 'YTU Blockchain',
+        image: '/img/partners/ytu-blockchain.png',
+        linkedin: 'https://www.linkedin.com/company/ytu-blockchain/',
+        x: 'https://x.com/ytu-blockchain',
+      },
+      {
+        name: 'İÜ BLOCKCHAIN',
+        image: '/img/partners/iu-blockchain.png',
+        linkedin: 'https://www.linkedin.com/company/iu-blockchain/',
+        x: 'https://x.com/iu-blockchain',
+      },
+      {
+        name: 'Çukurova Blockchain',
+        image: '/img/partners/cukurova-blockchain.png',
+        linkedin: 'https://www.linkedin.com/company/cukurova-blockchain/',
+        x: 'https://x.com/cukurova-blockchain',
+      },
+      {
+        name: 'ITU Blockchain',
+        image: '/img/partners/itu-blockchain.png',
+        linkedin: 'https://www.linkedin.com/company/itu-blockchain/',
+        x: 'https://x.com/itu-blockchain',
+      },
+      {
+        name: 'Subchain',
+        image: '/img/partners/subchain.png',
+        linkedin: 'https://www.linkedin.com/company/subchain/',
+        x: 'https://x.com/subchain',
+      },
+      {
+        name: 'Bilg',
+        image: '/img/partners/bilg.png',
+        linkedin: 'https://www.linkedin.com/company/bilg/',
+        x: 'https://x.com/bilg',
+      },
+    ]
+  },
+  media_partners: {
+    name: 'Media Partners',
+    logos: [
+      {
+        name: 'CDS',
+        image: '/img/partners/cds.png',
+        linkedin: 'https://www.linkedin.com/company/cds/',
+        x: 'https://x.com/cds',
+      },
+      {
+        name: 'Cointelegraph',
+        image: '/img/partners/cointelegraph.png',
+        linkedin: 'https://www.linkedin.com/company/cointelegraph/',
+        x: 'https://x.com/cointelegraph',
+      },
+      {
+        name: 'Coinkolik',
+        image: '/img/partners/coinkolik.png',
+        linkedin: 'https://www.linkedin.com/company/coinkolik/',
+        x: 'https://x.com/coinkolik',
+      },
+      {
+        name: 'Btc Haber',
+        image: '/img/partners/btc-haber.png',
+        linkedin: 'https://www.linkedin.com/company/btc-haber/',
+        x: 'https://x.com/btc-haber',
+      },
+    ]
+  },
+};
+
 module.exports = (req, res) => {
   return res.render('index/index', {
     page: 'index/index',
@@ -148,5 +291,6 @@ module.exports = (req, res) => {
     },
     speakers,
     schedule,
+    sponsors,
   });
 };
